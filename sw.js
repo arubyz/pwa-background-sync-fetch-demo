d96b6e7b5290791038807fef568043b8f63b5726
Jay Zybura <arubyz@gmail.com>
2023-05-23 16:12:23 -0500

    report when periodicsync event is received

--- sw.js
+++ sw.js
@@ -1,13 +1,28 @@
 // Change this value to force the cache to update
 const cacheName = "cache";
 
-self.addEventListener("install", event => {
+let manifest_counter = 1;
+
+const registration = self.registration;
+
+const channelName = 'test-channel';
+const channelSend = new BroadcastChannel(channelName);
+
+function report(result) {
+	channelSend.postMessage({ result });
+}
+
+async function fetch_content() {
+	report('periodicsync event received');
+}
+
+self.addEventListener('install', event => {
 	// Kick out the old service worker
 	self.skipWaiting();
 });
 
 self.addEventListener('periodicsync', (event) => {
 	if (event.tag === 'content-sync') {
-		//event.waitUntil(fetchContent());
+		event.waitUntil(fetch_content());
 	}
 });
