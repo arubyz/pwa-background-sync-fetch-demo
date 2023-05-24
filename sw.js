ae0334f103dd8c956a3153b836906fd2831201b8
Jay Zybura <arubyz@gmail.com>
2023-05-24 09:35:04 -0500

    extra logging for fetch events

--- sw.js
+++ sw.js
@@ -23,7 +23,11 @@ for (const key in self) {
         const eventType = key.substr(2);
 		report(`registering for service worker event ${eventType}`);
         self.addEventListener(eventType, event => {
-			report(`service worker ${eventType} event received received: ${JSON.stringify(event)}`);
+			if (key === 'fetch') {
+				report(`service worker FETCH event received received: (${event.request.url}) ${JSON.stringify(event.request)}`);
+			} else {
+				report(`service worker ${eventType} event received received: ${JSON.stringify(event)}`);
+			}
 		});
     }
 }
